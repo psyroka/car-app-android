a75d7fbc1898fd4468a34d52f8e67291541b1168
Patryk <syrokapatryk@gmail.com>
2020-11-25 18:36:27 +0100

    Add register layout

---
+++ app/src/main/java/car/app/RegisterActivity.java
@@ -0,0 +1,23 @@
+package car.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText username, password, email, city, dob;
+    Button register, cancle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        email = findViewById(R.id.email);
+        city = findViewById(R.id.city);
+        dob = findViewById(R.id.dob);
+    }
+}
